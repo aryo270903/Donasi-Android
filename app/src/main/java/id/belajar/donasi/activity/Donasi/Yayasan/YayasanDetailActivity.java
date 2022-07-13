af48518347a34f477561dd6669d20595b7644582
unknown <ariyo3477@gmail.com>
2022-07-13 15:05:15 +0700

    update

--- app/src/main/java/id/belajar/donasi/activity/Donasi/Yayasan/YayasanDetailActivity.java
+++ app/src/main/java/id/belajar/donasi/activity/Donasi/Yayasan/YayasanDetailActivity.java
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.view.View;
+import android.widget.AdapterView;
 import android.widget.Spinner;
 import android.widget.Toast;
 
@@ -48,6 +49,60 @@ public class YayasanDetailActivity extends AppCompatActivity {
             binding.Inputnotlp.setVisibility(View.GONE);
         }
 
+        binding.InputPengiriman.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                String value = (String) parent.getSelectedItem();
+                if (value.equals("Diambil")){
+                    binding.outlinedTextFieldProvinsi.setVisibility(View.VISIBLE);
+                    binding.outlinedTextFieldKota.setVisibility(View.VISIBLE);
+                    binding.outlinedTextFieldKecamatan.setVisibility(View.VISIBLE);
+                    binding.outlinedTextFieldKelurahan.setVisibility(view.VISIBLE);
+                    binding.outlinedTextFieldLatitude.setVisibility(view.VISIBLE);
+                } else {
+                    binding.outlinedTextFieldProvinsi.setVisibility(View.GONE);
+                    binding.outlinedTextFieldKota.setVisibility(View.GONE);
+                    binding.outlinedTextFieldKecamatan.setVisibility(View.GONE);
+                    binding.outlinedTextFieldKelurahan.setVisibility(view.GONE);
+                    binding.outlinedTextFieldLatitude.setVisibility(view.GONE);
+                }
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+
+            }
+        });
+        binding.InputJenisBarang.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                String value = (String) parent.getSelectedItem();
+                if(value.equals("Uang")){
+                    binding.spinner2pembayaran.setVisibility(View.VISIBLE);
+                    binding.spinner2.setVisibility(View.GONE);
+                    binding.outlinedTextFieldProvinsi.setVisibility(View.GONE);
+                    binding.outlinedTextFieldKota.setVisibility(View.GONE);
+                    binding.outlinedTextFieldKecamatan.setVisibility(View.GONE);
+                    binding.outlinedTextFieldKelurahan.setVisibility(view.GONE);
+                    binding.outlinedTextFieldLatitude.setVisibility(view.GONE);
+                } else {
+                    binding.spinner2.setVisibility(View.VISIBLE);
+                    binding.spinner2pembayaran.setVisibility(View.GONE);
+                    binding.outlinedTextFieldProvinsi.setVisibility(View.GONE);
+                    binding.outlinedTextFieldKota.setVisibility(View.GONE);
+                    binding.outlinedTextFieldKecamatan.setVisibility(View.GONE);
+                    binding.outlinedTextFieldKelurahan.setVisibility(view.GONE);
+                    binding.outlinedTextFieldLatitude.setVisibility(view.GONE);
+                }
+            }
+
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+
+            }
+        });
+
         binding.backdetailyayasan.setOnClickListener(v -> {
             finish();
         });
@@ -72,8 +127,10 @@ public class YayasanDetailActivity extends AppCompatActivity {
     }
 
     private void validasi(){
+        String name = binding.InputNama.getText().toString();
+        String email = binding.inputEmail.getText().toString();
+        String notlp = binding.Inputnotlp.getText().toString();
         String jenisbarang = binding.InputJenisBarang.getSelectedItem().toString();
-        String jumlah = binding.InputJumlah.getText().toString();
         String pengiriman = binding.InputPengiriman.getSelectedItem().toString();
         String provinsi = binding.InputProvinsi.getText().toString();
         String kota = binding.InputKota.getText().toString();
@@ -81,15 +138,17 @@ public class YayasanDetailActivity extends AppCompatActivity {
         String kelurahan = binding.InputKelurahan.getText().toString();
         String fulladdress = binding.Inputalamat.getText().toString();
 
-        submitDonasi(jenisbarang,jumlah,pengiriman,provinsi,kota,kecamatan,kelurahan,fulladdress);
+        submitDonasi(name,email,notlp,jenisbarang,pengiriman,provinsi,kota,kecamatan,kelurahan,fulladdress);
     }
 
-    void submitDonasi(String jenis_donasi, String jumlah, String pengiriman, String provinsi, String kota , String kecamatan,
+    void submitDonasi(String name,String email,String no_tlp,String jenis_donasi, String pengiriman, String provinsi, String kota , String kecamatan,
                       String kelurahan, String fulladdress){
 
         HashMap<String,String> request = new HashMap<>();
+        request.put("name",name);
+        request.put("email", email);
+        request.put("no_tlp",no_tlp);
         request.put("jenis_donasi",jenis_donasi);
-        request.put("jumlah",jumlah);
         request.put("pengiriman",pengiriman);
         request.put("provinsi",provinsi);
         request.put("kota",kota);
